3a7e395cc8109f0b38fea5b40dc74a8ae008598c
amirco99 <amirakefet@gmail.com>
2018-11-04 13:02:26 +0200

    Add files via upload

--- role.tf
+++ role.tf
@@ -3,12 +3,12 @@ resource "aws_iam_role" "ec2-s3_access_role" {
   assume_role_policy = "${file("${path.module}/user-data/assume-role-policy.json")}"
 }
 resource "aws_iam_instance_profile" "new_profile" {
-  name  = "ec2-s3-access-role"
+  name  = "ec2-s3-role"
   role = "${aws_iam_role.ec2-s3_access_role.name}"
 }
 
 resource "aws_iam_policy" "policy" {
-  name        = "new-policy"
+  name        = "mynew-policy"
   description = "A new policy"
   policy      = "${file("${path.module}/user-data/policy-s3-bucket.json")}"
 }
