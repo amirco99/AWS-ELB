eab66bc9e250cf14c376803d2da4353f0b2e5430
amirco99 <amirakefet@gmail.com>
2018-11-04 10:01:09 +0200

    Add files via upload

--- user-data/auto_user-data.sh
+++ user-data/auto_user-data.sh
@@ -6,18 +6,12 @@ set -e
 
 # Send the log output from this script to user-data.log, syslog, and the console
 # From: https://alestic.com/2010/12/ec2-user-data-output/
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+exec > >(tee /var/log/auto_user-data.log|logger -t auto_user-data -s 2>/dev/console) 2>&1
 
 # The variables below are filled in using Terraform interpolation
 echo "${instance_text}" > index.html
 sudo apt-get update
-#nohup busybox httpd -f -p "${instance_port}" &
-apt install awscli -y
-aws s3 cp --recursive s3://amirco99-bucket  / --region eu-west-2
 sudo apt-get -y install nginx
-#sudo service nginx start
-#export HOSTNAME=$(curl -s http://169.254.169.254/latest/metadata/hostname)
-#export $PUBLIC_IPV$=$(curl -s curl http://169.254.169.254/latest/meta-data/public-ipv4)
-#echo Hi from AWS $HOSTNAME, with IP Address: $PUBLIC_IPV > /var/www/html/indexx.nginx.html
+sudo service nginx start
 nohup busybox httpd -f -p "${instance_port}" &
 
